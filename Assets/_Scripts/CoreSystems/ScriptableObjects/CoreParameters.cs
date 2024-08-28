e43902a46c0f64a8448a11ad4eaa0b8451864719
SWoolfeek <50416553+SWoolfeek@users.noreply.github.com>
2024-08-29 01:24:11 +0200

    Tick system.

---
+++ Assets/_Scripts/CoreSystems/ScriptableObjects/CoreParameters.cs
@@ -0,0 +1,10 @@
+namespace CoreSystems
+{
+    using UnityEngine;
+    
+    public class CoreParameters : ScriptableObject
+    {
+        public float timeBetweenTicks = 1f; // Time in seconds.
+    }
+}
+
