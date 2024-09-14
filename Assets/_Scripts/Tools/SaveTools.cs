b901b5f1ef76da6af7f8ba8987879d6b274e838e
SWoolfeek <50416553+SWoolfeek@users.noreply.github.com>
2024-09-14 07:34:17 +0200

    SaveTools

--- Assets/_Scripts/Tools/SaveTools.cs
+++ Assets/_Scripts/Tools/SaveTools.cs
@@ -1,25 +1,23 @@
 namespace Tools
 {
+    
 #if UNITY_EDITOR
-    using System.Collections;
-    using System.Collections.Generic;
     using UnityEngine;
     using UnityEditor;
 
 
-    
     public class SaveTools
     {
-        private Saver.Saver _saver = new Saver.Saver();
+        private static Saver.Saver _saver = new Saver.Saver();
 
         [MenuItem("Tools/Save Tools/Clear Saves/All Saves")]
-        public void ClearAllSaves()
+        public static void ClearAllSaves()
         {
             Debug.Log("This method is not ready!");
         }
 
         [MenuItem("Tools/Save Tools/Clear Saves/Products")]
-        public void ClearProducts()
+        public static void ClearProducts()
         {
             _saver.ClearExactSaveFile(Saver.ESaveData.Products);
         }
